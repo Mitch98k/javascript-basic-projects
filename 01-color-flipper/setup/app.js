a5133943d6e8ab6dab83525a8549c4183f7531e6
Mitchell Knudsen <mitch@Mitchells-MBP.hsd1.ut.comcast.net>
2023-03-01 19:55:02 -0700

    Simple color picker completed

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const button = document.querySelector("button");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", function () {
+    const randomNumber = getRandom();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandom() {
+    return Math.floor(Math.random() * colors.length);
+}
